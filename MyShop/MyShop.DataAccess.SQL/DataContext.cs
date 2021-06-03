f14f2c872cbb4eed2ce8eeb5fd6168a582407b8e
safeer <safeer@DESKTOP-PET9FDS>
2021-06-03 12:21:14 +0800

    Basket Service

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -19,5 +19,10 @@ namespace MyShop.DataAccess.SQL
 
         public DbSet<ProductCategory> ProductCategories { get; set; }
 
+        public DbSet<Basket> Baskets { get; set; }
+
+        public DbSet<BasketItem> BasketItems { get; set; }
+
+
     }
 }
